55fd7d3cb11448853af9c5c30ad948b21a7ca602
Marek Stasikowski <marek.stasikowski@me.com>
2021-09-09 22:50:23 +0100

    Initialise Cucumber with config from environment

---
+++ README.md
@@ -0,0 +1,6 @@
+# Notes API
+
+## Setup
+
+1. `npm i`
+1. `cp .env.example .env`
--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,7 @@ module.exports = {
   default: [
     `--format-options '{"snippetInterface": "synchronous"}'`,
     '--require-module ts-node/register',
+    '--require src/features/support.ts',
     '--require src/**/step-definitions/*.ts'
   ].join(' ')
 };
