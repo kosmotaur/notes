091e6dd8f2cff3e053689a36214a67d88f5804a5
Marek Stasikowski <marek.stasikowski@me.com>
2021-09-10 18:52:37 +0100

    Mount handler to create notes
    This is behind a feature flag `ENABLE_ADDING_NOTES`

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,40 +3,56 @@ import express, { Express } from 'express';
 import { mocked } from 'ts-jest/utils';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import createPostNote from './routes/postNote';
+import mockClient from './client.mock';
 
 jest.mock('morgan');
 jest.mock('express');
 jest.mock('body-parser', () => ({
   json: jest.fn()
 }));
+jest.mock('./routes/postNote');
 
 const mockApp = jest.fn() as unknown as Express;
 const mockUse = jest.fn();
+const mockPost = jest.fn();
 const mockJsonBodyParser = jest.fn();
 const mockLoggingMiddleware = jest.fn();
+const mockPostNote = jest.fn();
 
 describe('application', () => {
   beforeEach(() => {
     jest.resetAllMocks();
     mockApp.use = mockUse;
+    mockApp.post = mockPost;
     mocked(express).mockReturnValue(mockApp);
     mocked(bodyParser.json).mockReturnValue(mockJsonBodyParser);
     mocked(morgan).mockReturnValue(mockLoggingMiddleware);
+    mocked(createPostNote).mockReturnValue(mockPostNote);
   });
   it('creates an application', () => {
-    const app = createApp();
+    const app = createApp(mockClient);
 
     expect(app).toBe(mockApp);
   });
   it('parses json request bodies', () => {
-    createApp();
+    createApp(mockClient);
 
     expect(mockUse).toHaveBeenCalledWith(mockJsonBodyParser);
   });
   it('logs requests', () => {
-    createApp();
+    createApp(mockClient);
 
     expect(morgan).toHaveBeenCalledWith('dev');
     expect(mockUse).toHaveBeenCalledWith(mockLoggingMiddleware);
   });
+  (process.env.ENABLE_ADDING_NOTES === 'true' ? it : it.skip)(
+    'mounts handler to create notes',
+    () => {
+      createApp(mockClient);
+
+      expect(createPostNote).toHaveBeenCalledWith(mockClient);
+      expect(mockPost).toHaveBeenCalledWith('/notes', mockPostNote);
+    }
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import { Client } from './client';
+import createPostNote from './routes/postNote';
 
-const createApp = (): Express => {
+const createApp = (client: Client): Express => {
   const app = express();
 
   app.use(morgan('dev'));
   app.use(bodyParser.json());
+
+  if (process.env.ENABLE_ADDING_NOTES === 'true') {
+    app.post('/notes', createPostNote(client));
+  }
 
   return app;
 };
