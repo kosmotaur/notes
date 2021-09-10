d00d7ad8ed5bb6c9c770b30f1a5b68ae47d5278d
Marek Stasikowski <marek.stasikowski@me.com>
2021-09-10 16:28:45 +0100

    Call next handler when creating a new note

--- src/routes/postNote.test.ts
+++ src/routes/postNote.test.ts
@@ -1,21 +1,25 @@
 import createRoute from './postNote';
-import { Note } from '../Note';
 import { Request, RequestHandler, Response } from 'express';
 import mockClient from '../client.mock';
+import { Note } from '@prisma/client';
 
 let route: RequestHandler;
 const mockNext = jest.fn();
+
+const mockNote: Note = {
+  title: 'my great note',
+  description: 'Lorem ipsum dolor sit amet',
+  ownerId: 42,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  id: 13
+};
 
 describe('create notes route', () => {
   beforeEach(() => {
     route = createRoute(mockClient);
   });
   it('creates a new note', () => {
-    const mockNote: Note = {
-      title: 'my great note',
-      description: 'Lorem ipsum dolor sit amet',
-      owner: 42
-    };
     const mockRequest = {
       body: mockNote
     } as Request;
@@ -24,5 +28,10 @@ describe('create notes route', () => {
     expect(mockClient.note.create).toHaveBeenCalledWith({
       data: mockNote
     });
+  });
+  it('calls next handler', () => {
+    route({} as Request, {} as Response, mockNext);
+
+    expect(mockNext).toHaveBeenCalledWith();
   });
 });
--- src/routes/postNote.ts
+++ src/routes/postNote.ts
@@ -1,12 +1,14 @@
-import { Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { Client } from '../client';
 
 const createPostNote =
   (client: Client) =>
-  (req: Request): void => {
+  (req: Request, res: Response, next: NextFunction): void => {
     client.note.create({
       data: req.body
     });
+
+    next();
   };
 
 export default createPostNote;
