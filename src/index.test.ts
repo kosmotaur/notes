15c83a5aea0b891beacac9d848a1dddc94d72107
Marek Stasikowski <marek.stasikowski@me.com>
2021-09-10 18:20:12 +0100

    Add logging middleware

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,9 @@ import createApp from './index';
 import express, { Express } from 'express';
 import { mocked } from 'ts-jest/utils';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
+jest.mock('morgan');
 jest.mock('express');
 jest.mock('body-parser', () => ({
   json: jest.fn()
@@ -11,6 +13,7 @@ jest.mock('body-parser', () => ({
 const mockApp = jest.fn() as unknown as Express;
 const mockUse = jest.fn();
 const mockJsonBodyParser = jest.fn();
+const mockLoggingMiddleware = jest.fn();
 
 describe('application', () => {
   beforeEach(() => {
@@ -18,6 +21,7 @@ describe('application', () => {
     mockApp.use = mockUse;
     mocked(express).mockReturnValue(mockApp);
     mocked(bodyParser.json).mockReturnValue(mockJsonBodyParser);
+    mocked(morgan).mockReturnValue(mockLoggingMiddleware);
   });
   it('creates an application', () => {
     const app = createApp();
@@ -28,5 +32,11 @@ describe('application', () => {
     createApp();
 
     expect(mockUse).toHaveBeenCalledWith(mockJsonBodyParser);
+  });
+  it('logs requests', () => {
+    createApp();
+
+    expect(morgan).toHaveBeenCalledWith('dev');
+    expect(mockUse).toHaveBeenCalledWith(mockLoggingMiddleware);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const createApp = (): Express => {
   const app = express();
 
+  app.use(morgan('dev'));
   app.use(bodyParser.json());
 
   return app;
