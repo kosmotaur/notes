d2f331579a5d756acdc46909b415c64fe351c595
Marek Stasikowski <marek.stasikowski@me.com>
2021-09-10 18:16:52 +0100

    Parse json request bodies

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,32 @@
 import createApp from './index';
 import express, { Express } from 'express';
 import { mocked } from 'ts-jest/utils';
+import bodyParser from 'body-parser';
 
 jest.mock('express');
+jest.mock('body-parser', () => ({
+  json: jest.fn()
+}));
+
+const mockApp = jest.fn() as unknown as Express;
+const mockUse = jest.fn();
+const mockJsonBodyParser = jest.fn();
 
 describe('application', () => {
-  it('creates an application', () => {
-    const mockApp = jest.fn() as unknown as Express;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockApp.use = mockUse;
     mocked(express).mockReturnValue(mockApp);
-
+    mocked(bodyParser.json).mockReturnValue(mockJsonBodyParser);
+  });
+  it('creates an application', () => {
     const app = createApp();
 
     expect(app).toBe(mockApp);
+  });
+  it('parses json request bodies', () => {
+    createApp();
+
+    expect(mockUse).toHaveBeenCalledWith(mockJsonBodyParser);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import express, { Express } from 'express';
+import bodyParser from 'body-parser';
 
-export default (): Express => express();
+const createApp = (): Express => {
+  const app = express();
+
+  app.use(bodyParser.json());
+
+  return app;
+};
+
+export default createApp;
