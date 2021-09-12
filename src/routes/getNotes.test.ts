43880109f84bc229d21924f4fdd91b0265ba9fb7
Marek Stasikowski <marek.stasikowski@me.com>
2021-09-12 23:06:16 +0100

    Implement sending back notes in get notes route

--- src/routes/getNotes.test.ts
+++ src/routes/getNotes.test.ts
@@ -1,7 +1,40 @@
 import createRoute from './getNotes';
+import mockClient from '../client.mock';
+import { Note } from '@prisma/client';
+import { Request, Response } from 'express';
+
+const mockRequest = {} as Request;
+const mockResponse = {
+  json: jest.fn()
+} as unknown as Response;
+
+const mockNotes: Note[] = [
+  {
+    id: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    description: 'foo bar',
+    title: 'qux doo',
+    ownerId: 1
+  },
+  {
+    id: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    description: 'lorem ipsum',
+    title: 'asd qwe zxc',
+    ownerId: 1
+  }
+];
 
 describe('get notes route', () => {
-  it('is a function', () => {
-    createRoute();
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  it('sends back all notes', async () => {
+    mockClient.note.findMany.mockResolvedValue(mockNotes);
+    await createRoute(mockClient)(mockRequest, mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledWith(mockNotes);
   });
 });
--- src/routes/getNotes.ts
+++ src/routes/getNotes.ts
@@ -1,3 +1,10 @@
-const createGetNotes = (): void => {};
+import { Client } from '../client';
+import { Request, Response } from 'express';
+
+const createGetNotes =
+  (client: Client) =>
+  async (req: Request, res: Response): Promise<void> => {
+    res.json(await client.note.findMany());
+  };
 
 export default createGetNotes;
